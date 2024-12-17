015a3e243648c26a8ab6700096925e8753b2e897
JonathanLuher <129464379+JonathanLuher@users.noreply.github.com>
2024-12-17 15:38:10 -0600

    Dark background
    In this commit I fixed the dark background in the login component

--- BD/BD_mathkids.sql
+++ BD/BD_mathkids.sql
@@ -1,11 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `mathkids` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+-- Crear base de datos
+CREATE DATABASE IF NOT EXISTS `mathkids` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `mathkids`;
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
---
--- Host: localhost    Database: mathkids
--- ------------------------------------------------------
--- Server version	8.0.37
 
+-- Configuración inicial
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -17,10 +14,7 @@ USE `mathkids`;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `questions`
---
-
+-- Tabla 'questions'
 DROP TABLE IF EXISTS `questions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -33,28 +27,28 @@ CREATE TABLE `questions` (
   `option_3` int NOT NULL,
   `option_4` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `questions`
---
-
+-- Insertar datos en 'questions'
 LOCK TABLES `questions` WRITE;
 /*!40000 ALTER TABLE `questions` DISABLE KEYS */;
-INSERT INTO `questions` VALUES (21,'5 + 3',8,8,7,10,6),(22,'10 - 4',6,6,5,7,8),(23,'7 * 2',14,14,12,15,13),(24,'12 / 4',3,3,2,4,5),(25,'9 + 6',15,15,14,16,13),(26,'8 * 5',40,40,35,45,50),(27,'14 - 7',7,7,8,6,5),(28,'20 / 5',4,4,3,5,6),(29,'11 + 12',23,23,21,24,25),(30,'6 * 3',18,18,16,20,22);
+INSERT INTO `questions` VALUES 
+(21,'5 + 3',8,8,7,10,6),
+(22,'10 - 4',6,6,5,7,8),
+(23,'7 * 2',14,14,12,15,13),
+(24,'12 / 4',3,3,2,4,5),
+(25,'9 + 6',15,15,14,16,13),
+(26,'8 * 5',40,40,35,45,50),
+(27,'14 - 7',7,7,8,6,5),
+(28,'20 / 5',4,4,3,5,6),
+(29,'11 + 12',23,23,21,24,25),
+(30,'6 * 3',18,18,16,20,22);
 /*!40000 ALTER TABLE `questions` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Dumping events for database 'mathkids'
---
-
---
--- Dumping routines for database 'mathkids'
---
+-- Restaurar configuración inicial
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
@@ -62,5 +56,3 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-12-13  2:44:57
